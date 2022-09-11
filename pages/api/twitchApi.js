9e7fdf45d105f8db1c067cde769d9207ce593435
HsuTingHuan <s780609@gmail.com>
2022-09-11 11:18:32 +0800

    WIP: debug...remove async

--- pages/api/twitchApi.js
+++ pages/api/twitchApi.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 var https = require("https");
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 
   // if (req.method === "GET") {
